14e7ac807cf280a47fef3433680a45d0fa7fe0c8
doctori <lerouge.pierre@gmail.com>
2013-09-25 23:14:35 +0200

    updating the port string connexion

--- server.js
+++ server.js
@@ -2,13 +2,14 @@ function start(route, handle){
 	var restify = require('restify');
 	var requestHandlers = require('./requestHandlers');
 	var server = restify.createServer();
+	var port = process.env.port || 1337;
 	server.use(restify.bodyParser());
 	server.use(restify.fullResponse());
 	server.get('/messages/:hiker',requestHandlers.getMessages)
 	server.get('/messages',requestHandlers.getMessages);
 	server.post('/messages',requestHandlers.postMessage);
 
-	server.listen(80, function(){
+	server.listen(port, function(){
 		console.log('%s listenning at %s',server.name, server.url);
 	});
 }
