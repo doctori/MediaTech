6f8a19bd9a64d37f55f8440062ddbdd66a93bb75
doctori <lerouge.pierre@gmail.com>
2013-09-25 21:08:54 +0200

    removing old router

--- index.js
+++ index.js
@@ -1,8 +1,4 @@
 var server = require("./server"),
     router = require("./router"),
-    requestHandlers = require("./requestHandlers"),
-    handle = {};
-handle["/"] = requestHandlers.start;
-handle["/start"] = requestHandlers.start;
-handle["/upload"] = requestHandlers.upload;
-server.start(router.route, handle);
+    requestHandlers = require("./requestHandlers")
+	server.start();
